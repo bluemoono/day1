85461edcb63583e74f8325dbfde23f9c0cdd2a2e
Bluemoono <mikka_paulina@live.com>
2021-10-20 20:48:20 -0300

    Trabalhos da kenzi - até 20/10/2021

---
+++ Node/01-script/script.js
@@ -0,0 +1,30 @@
+//console.log("Hello Word!");
+
+//Aula 34
+/*for (let index = 0; index <= 10; index++) {
+    console.log(index);
+}
+
+for (let index = 0; index <= 10; index+=2) {
+    console.log(index);
+}
+
+for (let index = 10; index >= 0; index--) {
+    console.log(index);
+}
+
+for (let index = 10; index >= 0; index-=2) {
+    console.log(index);
+}
+
+
+while(i<=10){
+    console.log(i)
+    i+=2;
+}*/
+let i=0
+while(i<10){
+    i++
+    console.log(i)
+    i++
+}
